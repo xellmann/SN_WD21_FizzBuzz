4f579646bf005e31cf2965d16f26e0a565f9b62e
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-12 18:17:40 +0200

    while loop added

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,18 @@
 print("Herzlich willkommen zu unserem FizzBuzz!")
-zahl1 = int(input("Bitte geben Sie eine Zahl zwischen 1 und 100 ein: "))
-print("Die ausgewählte Zahl lautet: " + str(zahl1))
 
-for i in range(1, zahl1+1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print(i)
+while True:
+    zahl1 = int(input("Bitte geben Sie eine Zahl zwischen 1 und 100 ein: "))
+    print("Die ausgewählte Zahl lautet: " + str(zahl1))
+
+    for i in range(1, zahl1+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+    cont = input("Möchtest du weiter machen? (JA | NEIN)")
+    if (cont.upper() == "NEIN"):
+        break
